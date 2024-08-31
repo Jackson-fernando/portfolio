a26cc2b8078013009d763287524b1c62418776ef
jackson <142001335+Jackson-fernando@users.noreply.github.com>
2024-08-31 10:45:40 +0530

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,60 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import bootstrap from "./tech/bootstrap.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import java from "./tech/java.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import sqlserver from "./tech/sqlserver.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+import inbox from "./company/inbox.png";
+import dneg from "./company/dneg.png";
+import vfxstudio from "./company/vfxstudio.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  bootstrap,
+  figma,
+  git,
+  html,
+  javascript,
+  java,
+  nodejs,
+  reactjs,
+  sqlserver,
+  tailwind,
+  typescript,
+  threejs,
+  inbox,
+  dneg,
+  vfxstudio,
+  carrent,
+  jobit,
+  tripguide,
+};
